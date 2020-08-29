79f1bfa75611bc05332e16448155d9a119b28b55
Chris Maxwell <chmaxwel@icloud.com>
2020-08-29 13:51:53 +0200

    updated login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -49,4 +49,9 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
 module.exports = router;
