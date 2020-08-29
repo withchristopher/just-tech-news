16da147543cce97ccc2ed5fa1fed94b9b3311c69
Chris Maxwell <chmaxwel@icloud.com>
2020-08-29 17:21:12 +0200

    added login/logout fn

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -96,12 +96,14 @@ router.post('/', (req, res) => {
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
     // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    if (req.session) {
+        Post.upvote({...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedVoteData => res.json(updatedVoteData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 });
 
 router.put('/:id', (req, res) => {
