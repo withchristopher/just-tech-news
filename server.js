c7b6b39b4847e3e8d3b0d60343dd213d2a4cc912
Chris Maxwell <chmaxwel@icloud.com>
2020-08-29 09:52:36 +0200

    move to exphbs and hbs

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// handlebars setup
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+// Setup HBS engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'))); // Built-in Express.JS middleware that takes all contents of a folder and serve as assets 
 
 // turn on routes
 app.use(routes);
