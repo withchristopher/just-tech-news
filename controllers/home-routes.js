ea631541e8586471e2a10e9853707ffd19ba0e8a
Chris Maxwell <chmaxwel@icloud.com>
2020-08-30 17:33:51 +0200

    withAuth added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 // router.get('/', (req, res) => {
 //     res.render('homepage', {
@@ -16,7 +17,7 @@ const { Post, User, Comment } = require('../models');
 //     });
 // });
 
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
     console.log(req.session);
     Post.findAll({
             attributes: [
@@ -59,7 +60,7 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-router.get('/post/:id', (req, res) => {
+router.get('/post/:id', withAuth, (req, res) => {
     Post.findOne({
             where: {
                 id: req.params.id
---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// This function acts a normal request callback function, check for existence of a session property
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
